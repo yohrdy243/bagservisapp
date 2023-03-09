18893234bf449698791e3f4d4cfc2d4b89569c13
yohrdy243 <yohrdy243@gmail.com>
2023-03-09 12:36:28 -0500

    Stage 001

---
+++ screens/app-settings/App-settings.jsx
@@ -0,0 +1,12 @@
+import { Text } from "native-base";
+import { View } from "react-native";
+
+const AppSettings = () => {
+  return (
+    <View>
+      <Text bold>This is the App Settings</Text>
+    </View>
+  );
+};  
+
+export default AppSettings;
